35e5b1446c971a50226f41f91b0243f35242de10
trankienhcl <121598703+trankienhcl@users.noreply.github.com>
2023-03-01 15:36:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express(); 
 
 app.get("/", (req, res) => { 
-  res.send("This is my express app"); 
+  res.send("Hello mtfk"); 
 }); 
 
 app.get("/me", (req, res) => { 
