15a664c6954a89ed74664b279bc26bec0ab40248
trankienhcl <121598703+trankienhcl@users.noreply.github.com>
2023-03-02 12:50:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const app = express(); 
 
 app.get("/", (req, res) => { 
-  res.send("Hello mtfk"); 
+  res.send("Hello there"); 
+  res.send("welcome to playback session");
 }); 
 
 app.get("/me", (req, res) => { 
   res.send("Hi I am kaye"); 
+  res.send("kaye of hallownest");
 }); 
 
 app.listen(5000, () => { 
